a8f570f2883832d4e2ce3681a24a35119ca1bd79
Ashley <ashley@fishtownanalytics.com>
2020-10-07 13:45:54 -0400

    pr edits: css variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "dbt-orange": "#ff694b",
+        gray: {
+          lighter: "#f9fafb",
+          dark: "#a9a9a9",
+          400: "#cbd5e0",
+          500: "#a0aec0"
+        },
+        orange: {
+          default: "#ff694b", // "dbt-orange"
+        },
+        red: {
+          default: "#e21b10",
+        },
       },
     },
   },
