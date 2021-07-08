36059390904856dc9890fe6789260b26a5e749d2
Andrew Sweet <andrewmsweet@gmail.com>
2021-07-08 08:18:30 -0400

    Adding DAG colors as semantic color names in FUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,42 @@ module.exports = {
           ...colors.red,
           DEFAULT: 'var(--color-red)',
         },
+        'dag-node-selected': 'var(--color-dag-node-selected)',
+        'dag-node-exposure': {
+          400: 'var(--color-dag-node-exposure-400)',
+          500: 'var(--color-dag-node-exposure-500)',
+          600: 'var(--color-dag-node-exposure-600)',
+        },
+        'dag-node-model': {
+          400: 'var(--color-dag-node-model-400)',
+          500: 'var(--color-dag-node-model-500)',
+          600: 'var(--color-dag-node-model-600)',
+        },
+        'dag-node-test': {
+          400: 'var(--color-dag-node-test-400)',
+          500: 'var(--color-dag-node-test-500)',
+          600: 'var(--color-dag-node-test-600)',
+        },
+        'dag-node-analysis': {
+          400: 'var(--color-dag-node-analysis-400)',
+          500: 'var(--color-dag-node-analysis-500)',
+          600: 'var(--color-dag-node-analysis-600)',
+        },
+        'dag-node-source': {
+          400: 'var(--color-dag-node-source-400)',
+          500: 'var(--color-dag-node-source-500)',
+          600: 'var(--color-dag-node-source-600)',
+        },
+        'dag-node-seed': {
+          400: 'var(--color-dag-node-seed-400)',
+          500: 'var(--color-dag-node-seed-500)',
+          600: 'var(--color-dag-node-seed-600)',
+        },
+        'dag-node-snapshot': {
+          400: 'var(color-dag-node-snapshot-400)',
+          500: 'var(color-dag-node-snapshot-500)',
+          600: 'var(color-dag-node-snapshot-600)',
+        },
       },
       fontSize,
     },
